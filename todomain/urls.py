4bb5b92f7720281c8b35c6bc23cc80898cd8bc1c
Agnishubh4 <agnihotrishubham44@gmail.com>
2025-10-03 12:43:00 +0530

    todo app initialized

--- todomain/urls.py
+++ todomain/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for todo_main project.
+URL configuration for todomain project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
